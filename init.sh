64ca459a1b221fdb9b71c5cc16e000c414951b5f
shibata <shibatachh@stf.teu.ac.jp>
2023-01-26 17:40:41 +0900

    second

--- init.sh
+++ init.sh
@@ -1,1 +1,4 @@
+#!/bin/bash                                                                                                                     
+set -x #echo on                                                                                                                 
+cp .zshrc .screenrc ~/
 curl https://pyenv.run | bash
