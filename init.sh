6ddb449590d17f92f3e950c7ca7be3fd8627cc04
shibata <shibatachh@stf.teu.ac.jp>
2023-01-26 17:53:05 +0900

    4

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash                                                                                                                     
 set -x #echo on                                                                                                                 
 cp .zshrc .screenrc ~/
+ssh-keygen -b 2048 -t rsa -q -N ""
 curl https://pyenv.run | bash
