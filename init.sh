eb3354425e83381248563a1a99f52a2393a7b838
CS <you@example.com>
2023-01-27 11:41:22 +0900

    6

--- init.sh
+++ init.sh
@@ -2,11 +2,16 @@
 set -x #echo on    
 
 #copy setting files                                                                                                            
-cp .zshrc .screenrc ~/
+cp -n .zshrc .screenrc ~/
 #set ssh 
-ssh-keygen -t rsa -N ""
-touch ~/.ssh/authorized_keys 
-chmod 600 ~/.ssh/authorized_keys  
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+FILE=~/.ssh/id_rsa
+
+if [ ! -e $FILE ]  ; then
+  ssh-keygen -t rsa -N ""
+  cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+  chmod 600 ~/.ssh/authorized_keys  
+else
+  echo $FILE already exists. ssh-keygen is skipped.
+fi
 #pyenv installation
 curl https://pyenv.run | bash
